609f95a33118455806ddeba81bd0d0c0ce53945a
d-mcalpin <duncan.mcalpin@gmail.com>
2021-03-27 23:51:19 +0000

    Added wishlist models and admin file

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,3 +1,12 @@
+from django.db import models
 from django.contrib import admin
+from .models import Wishlist
 
-# Register your models here.
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = (
+        "user_profile",
+    )
+
+
+admin.site.register(Wishlist, WishlistAdmin)
--- wishlists/models.py
+++ wishlists/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from products.models import Product
+from profiles.models import UserProfile
 
-# Create your models here.
+
+class Wishlist(models.Model):
+    user_profile = models.ForeignKey(
+        'profiles.UserProfile',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name="wishlist"
+        )
+    products = models.ManyToManyField('products.Product')
+
+    def __str__(self):
+        return f"{self.user_profile}'s wishlist"
