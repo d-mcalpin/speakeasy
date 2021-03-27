75314f0ebe7191e9ed717044a35db4096209a17f
d-mcalpin <duncan.mcalpin@gmail.com>
2021-03-27 23:41:08 +0000

    Added view to create user wishlists

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from profiles.models import UserProfile
+from products.models import Product
+from .models import Wishlist
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def wishlist(request):
+    """
+    Get and display user's wishlist if they have one created or create one if
+    they have not
+    """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    wishlist, created = Wishlist.objects.get_or_create(user_profile=profile)
+    wishlisted_products = Product.objects.filter(
+        wishlist__user_profile=profile)
+
+    context = {
+        "profile": profile,
+        "wishlisted_products": wishlisted_products,
+    }
+
+    return render(request, "wishlists/wishlist.html", context)
+
+
