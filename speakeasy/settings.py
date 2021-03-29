46e4119eed0a70b851f766d2371b905abe8cadde
d-mcalpin <duncan.mcalpin@gmail.com>
2021-03-29 11:32:53 +0000

    Updated secret key to run app

--- speakeasy/settings.py
+++ speakeasy/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'e)-7wip-@11p)djn=5kklfi(1dedc*$qyz#c5xz!dq!x2p4z0h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
